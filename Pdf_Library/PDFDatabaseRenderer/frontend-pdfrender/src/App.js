4ae0ae2d7288b62669533622616aaf91ea6a4d0a
Vinayak Raj Ranjan <vinayakrajqaz@gmail.com>
2024-07-01 17:22:21 +0530

    Add Frontend Code for PdfDatabase Rendering
    Signed-off-by: Vinayak Raj Ranjan <vinayakrajqaz@gmail.com>

---
+++ Pdf_Library/PDFDatabaseRenderer/frontend-pdfrender/src/App.js
@@ -0,0 +1,95 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { pdfjs } from "react-pdf";
+import PdfComp from "./PdfComp";
+import "./App.css"; 
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString();
+
+function App() {
+  const [title, setTitle] = useState("");
+  const [file, setFile] = useState(null);
+  const [allImage, setAllImage] = useState(null);
+  const [pdfFile, setPdfFile] = useState(null);
+
+  useEffect(() => {
+    getPdf();
+  }, []);
+
+  const getPdf = async () => {
+    const result = await axios.get("http://localhost:5000/get-files");
+    console.log(result.data.data);
+    setAllImage(result.data.data);
+  };
+
+  const submitImage = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("file", file);
+    console.log(title, file);
+
+    const result = await axios.post(
+      "http://localhost:5000/upload-files",
+      formData,
+      {
+        headers: { "Content-Type": "multipart/form-data" },
+      }
+    );
+    console.log(result);
+    if (result.data.status === "ok") {
+      alert("Uploaded Successfully!!!");
+      getPdf();
+    }
+  };
+
+  const togglePdf = (pdf) => {
+    if (pdfFile === `http://localhost:5000/files/${pdf}`) {
+      setPdfFile(null); // Hide PDF if already shown
+    } else {
+      setPdfFile(`http://localhost:5000/files/${pdf}`); // Show PDF
+    }
+  };
+
+  const downloadPdf = () => {
+    const link = document.createElement("a");
+    link.href = pdfFile;
+    link.download = "download.pdf";
+    link.click();
+  };
+
+  return (
+    <div className="App">
+      <div className="uploaded">
+        <h4>Uploaded PDF:</h4>
+        <div className="output-div">
+          {allImage == null
+            ? ""
+            : allImage.map((data) => (
+                <div className="inner-div" key={data.pdf}>
+                  <h6>Title: {data.title}</h6>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => togglePdf(data.pdf)}
+                  >
+                    {pdfFile === `http://localhost:5000/files/${data.pdf}`
+                      ? "Hide Pdf"
+                      : "Show Pdf"}
+                  </button>
+                </div>
+              ))}
+        </div>
+      </div>
+      <div className="pdf-container">
+        <PdfComp pdfFile={pdfFile} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
